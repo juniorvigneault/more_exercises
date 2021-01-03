79002b8b499eb6824ac8e7c59f3de83b714fd8aa
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2021-01-03 11:14:07 -0500

    Chapter 1 : Created a "W" using Bezier Vertex function

--- chapter1/js/script.js
+++ chapter1/js/script.js
@@ -4,7 +4,26 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(800, 800)
+  createCanvas(1000, 1000)
+  background(0);
+  stroke(255);
+  strokeWeight(50);
+  noFill();
+  // beginShape();
+  strokeCap(SQUARE);
+  // vertex(40, 760)
+  // vertex(40, 40);
+  // bezierVertex(40, 400, 360, 360, 360, 760);
+  // vertex(360,40);
+  // endShape();
+
+  beginShape();
+  vertex(300,200)
+  bezierVertex(300, 400, 400, 700, 400, 700);
+  bezierVertex(400, 700, 400, 500, 500, 500);
+  bezierVertex(600, 500, 600, 700, 600, 700);
+  bezierVertex(600, 700, 700, 400, 700, 200);
+  endShape();
 }
 
 function draw() {
