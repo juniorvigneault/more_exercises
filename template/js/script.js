32e2eb11635385880c3fd38cd48e1974d206aee5
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2021-02-07 12:16:02 -0500

    Template: Updated template file

--- template/js/script.js
+++ template/js/script.js
@@ -1,8 +1,27 @@
 "use strict";
 
-// exrecise by just junior
 
 function setup() {
   createCanvas(1000, 1000)
+}
+
+function draw() {
   background(0);
+}
+
+
+// functions to translate RGB to HSLuv for fill and stroke
+function fillHsluv(h, s, l) {
+  const rgb = hsluv.hsluvToRgb([h, s, l]);
+  fill(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
+function strokeHsluv(h, s, l) {
+  const rgb = hsluv.hsluvToRgb([h, s, l]);
+  stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
+// resizes the canvas
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
